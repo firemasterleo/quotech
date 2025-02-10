0892aa9cf8f3f826b4fc4349f10221347821065d
firemasterleo <firemasterleo@outlook.com>
2025-02-10 13:47:58 -0500

    Update imageStore.js

--- src/stores/imageStore.js
+++ src/stores/imageStore.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useImageStore = defineStore("image", {
   state: () => ({
-    heroImage: "/src/assets/hero-piccc.png", // Default image path
+    heroImage: "../assets/hero-piccc.png", // Default image path
   }),
   actions: {
     setHeroImage(imageUrl) {
