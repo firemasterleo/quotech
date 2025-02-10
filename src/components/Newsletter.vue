177b9da82e3e540e9a5952e0bdbf0fbf1db9daa4
firemasterleo <firemasterleo@outlook.com>
2025-02-09 19:24:47 -0500

    Update Newsletter.vue

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -7,6 +7,14 @@
   
       <button type="submit">Subscribe</button>
     </form>
+    <form name="newsletter" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="newsletter" value="contact" />
+      
+      <label for="email">Email Address:</label>
+      <input type="email" id="email" name="email" required>
+  
+      <button type="submit">Subscribe</button>
+    </form>
   </template>
 
 
